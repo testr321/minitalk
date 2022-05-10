6bcb6802d563885f8948f66abf8bc015c44bf745
Snowyz3 <bryanbongzihao@gmail.com>
2022-05-11 03:44:30 +0800

    11/5/2022

--- srcs/server.c
+++ srcs/server.c
@@ -2,40 +2,47 @@
 #include <signal.h>
 #include "../ft_printf/includes/ft_printf.h"
 
-void handleSigs(int sig)
+static void	handleSigs(int sig, siginfo_t *info, void *data)
 {
-	static int i;
-	static char c;
-	int	bit;
+	static int	i;
+	int			bit;
+	static int	c;
 
-	if (sig == SIGUSR1)
+	(void)data;
+	if (sig == SIGUSR2)
 		bit = 0;
 	else
 		bit = 1;
-
 	c += (bit << i);
-	i++;
+	i ++;
 	if (i == 8)
 	{
+		write(1, &c, 1);
 		if (c == '\0')
+		{
 			write(1, "\n", 1);
-		else
-			write(1, &c, 1);
-		c = 0;
+			kill(info->si_pid, SIGUSR1);
+		}
 		i = 0;
+		c = 0;
 	}
 }
 
-int main()
+int	main(void)
 {
+	pid_t				pid;
 	struct sigaction	sa;
 
-	ft_printf("SERVER PID: %d\n", getpid());
-	sa.sa_handler = handleSigs;
+	sa.sa_sigaction = handleSigs;
+	sa.sa_flags = SA_SIGINFO;
+	pid = getpid();
+	ft_putstr_fd("Talk to me!\n", 1);
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
-
 	return (0);
 }
